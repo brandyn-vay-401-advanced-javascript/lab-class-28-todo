458c506ba5ef3efc6bbb2e797a57f3e3aa58a516
Brandyn <37010312+KlNGU@users.noreply.github.com>
2019-09-12 18:51:46 -0700

    need to still write tests

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# lab-class-28-todo
+# LAB - 28 todo
+
+## Props and State
+
+### Author: Brandyn Vay
+
+### Links and Resources
+
+- [code sandbox](https://codesandbox.io/s/lab-class-28-todo-zbguf)
+
+### Modules
+
+#### `app.js`
+
+#### `todo.js`
+
+#### `form.js`
+
+#### `header.js`
+
+#### Tests
+
+- How do you run tests?
+- What assertions were made?
+- What assertions need to be / should be made?
+
+#### UML
+
+Link to an image of the UML for your application and response to events
---
+++ src/app.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Form from "./components/form/form.js";
+import Header from "./components/header/header.js";
+import List from "./components/todo/todo.js";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      items: ["First Item"]
+    };
+  }
+
+  addIt = string => {
+    this.setState({ items: [...this.state.items, string] });
+  };
+
+  deleteIt = idx => {
+    this.state.items.splice(idx, 1);
+    this.setState({ items: this.state.items });
+  };
+
+  render() {
+    return (
+      <>
+        <Header title="The To Do App" />
+        <Form action={this.addIt} items={this.state.items} />
+        <List action={this.deleteIt} items={this.state.items} />
+      </>
+    );
+  }
+}
+
+export default App;
